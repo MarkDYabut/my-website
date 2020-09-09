e44e43f8d7cac69797e02da3c4551f13c061b586
mark <yabut@Kijijis-MacBook-Pro.local>
2020-09-09 18:47:54 -0400

    contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-
-import LandingBio from "../components/landing-bio";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
