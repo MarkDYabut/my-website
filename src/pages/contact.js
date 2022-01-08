a4ceb6d62cbdb6ba6dc8e894e40942a5030301e7
mdotyabut <mark.yabut@ryerson.ca>
2022-01-08 17:40:03 -0500

    update contact to have layout instead

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,10 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import styled from "@emotion/styled";
 import { Global, css } from "@emotion/core";
-import { Link } from "gatsby";
 
 const Wrapper = styled.div`
-  margin-top: 30px;
-  margin-bottom: 50px;
+  margin-top: 40px;
+  margin-bottom: 20px;
   
 
   html {
@@ -57,10 +56,6 @@ const StyledInput = styled.input`
   ${sharedStyles}
 `;
 
-const NavLink = styled(Link)`
-  padding: 0 0.3rem 0;
-`;
-
 const StyledButton = styled.button`
   display: block;
   background-color: #f7797d;
@@ -91,20 +86,6 @@ const StyledFieldset = styled.fieldset`
   input {
     margin-right: 10px;
   }
-`;
-
-const RegLink = styled.a`
-  padding: 0 1.0875rem 1rem;
-`;
-
-const Footer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const BottomLinks = styled.div`
-  display: flex;
-  justify-content: center;
 `;
 
 const StyledTextArea = styled.textarea`
@@ -151,7 +132,7 @@ const IndexPage = () => {
 
   return (
     <>
-      <>
+      <Layout>
         <Wrapper>
           <StyledFormWrapper>
           <StyledForm
@@ -206,40 +187,7 @@ const IndexPage = () => {
           </StyledForm>
           </StyledFormWrapper>
         </Wrapper>
-
-        <hr />
-
-        <BottomLinks>
-          {/* <RegLink href="https://my-pomodoro.netlify.app/" target="_blank">
-            Tools
-          </RegLink> */}
-          <RegLink
-            href="https://www.linkedin.com/in/mark-yabut-218018152/"
-            target="_blank"
-          >
-            Linkedin
-          </RegLink>
-          <RegLink
-            href="https://github.com/MarkDYabut/my-website"
-            target="_blank"
-          >
-            Source Code
-          </RegLink>
-          <RegLink
-            href="https://www.instagram.com/markyabut3/"
-            target="_blank"
-          >
-            Instagram
-          </RegLink>
-          {/* <NavLink to="/youtube">Youtube</NavLink> */}
-
-        </BottomLinks>
-        
-        <Footer>
-            Built by <NavLink to="/tech/tech-post-2">Mark Yabut</NavLink>
-        </Footer>
-
-      </>
+      </Layout>
     </>
   );
 };
