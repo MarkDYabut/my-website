8f6253814a375f7a0f0f2be63776836658b8a189
mdotyabut <yabutmark@outlook.com>
2022-10-07 13:20:44 -0400

    iframes make it extremely easy to embed my other apps, check out the /poc route

--- src/pages/poc.js
+++ src/pages/poc.js
@@ -10,12 +10,19 @@ const Content = styled.div`
   padding: 1.45rem 1.0875rem;
 `;
 
+const Frame = styled.iframe`
+  border-style: none;
+  width: 100%;
+  height 600px;
+`
+
 const Poc = () => (
   <>
     <Layout>
       <Content>
-        <h1>Jokes...</h1>
+        <h1>Proofs of concepts aka. POC</h1>
         <p>Coming soon though</p>
+        <Frame src="https://my-pomodoro.netlify.app/"></Frame>
       </Content>
     </Layout>
   </>
